f3596e0b44dac9f415464d24480d75a93d5ef66d
dgandle <doug.gandle@gmail.com>
2018-06-12 15:48:09 -0400

    initial commit

---
+++ DGModalStatus/DGModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  DGModalStatus.h
+//  DGModalStatus
+//
+//  Created by Douglas Gandle on 6/12/18.
+//  Copyright © 2018 Douglas Gandle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGModalStatus.
+FOUNDATION_EXPORT double DGModalStatusVersionNumber;
+
+//! Project version string for DGModalStatus.
+FOUNDATION_EXPORT const unsigned char DGModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGModalStatus/PublicHeader.h>
+
+
---
+++ DGModalStatus/DGModalStatusView.swift
@@ -0,0 +1,60 @@
+//
+//  DGModalStatusView.swift
+//  DGModalStatus
+//
+//  Created by Douglas Gandle on 6/12/18.
+//  Copyright © 2018 Douglas Gandle. All rights reserved.
+//
+
+import UIKit
+
+public class DGModalStatusView: UIView {
+
+    @IBOutlet private weak var statusImage: UIImageView!
+    @IBOutlet private weak var headlineLabel: UILabel!
+    @IBOutlet private weak var subheadLabel: UILabel!
+    
+    let nibName = "AOModalStatusView"
+    var contentView: UIView!
+    
+    // MARK: Set Up View
+    public override init(frame: CGRect) {
+        // For use in code
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        // For use in Interface Builder
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        headlineLabel.text = ""
+        subheadLabel.text = ""
+    }
+
+    // Provide functions to update view
+    public func set(image: UIImage) {
+        self.statusImage.image = image
+    }
+    
+    public func set(headline text: String) {
+        self.headlineLabel.text = text
+    }
+    
+    public func set(subheading text: String) {
+        self.subheadLabel.text = text
+    }
+    
+}
